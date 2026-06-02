d1d652bd84e6e85e63a3bf70814d8cfaff0d043a
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-02 05:37:09 +0530

    🚀 My First Commit — Java basics started

---
+++ src/secondjune.java
@@ -0,0 +1,95 @@
+import java.util.*;
+public class secondjune {
+    public static void main(String args[]){
+/*       Scanner changekrskteh = new Scanner(System.in);
+
+        System.out.println(" enter number 1  ");
+        int numberone = changekrskteh.nextInt();
+
+        System.out.print("enter number 2 ");
+        int  numbertwo = changekrskteh.nextInt();
+
+        int product = numberone + numbertwo;
+        System.out.println("the product of two number is -");
+        System.out.println(product);
+*/
+        /*
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number ");
+        float a = sc.nextFloat();
+        int b = (int) a;
+        System.out.println("the product of two number is -");
+        System.out.println(b);
+
+         */
+        /* character k liye dekh rha h
+
+        Scanner sc = new Scanner(System.in);
+        char ch = 'p';
+        int number = ch;
+        System.out.println(number);
+        */
+        /*
+        byte b = 3;
+        byte a = (byte) (b+3) ;
+        System.out.println(a);
+
+         */
+      /*
+        Question1:Inaprogram,input3numbers:A,BandC
+        You have to output the average of these 3 numbers.
+        (Hint : Average of N numbers is sum of those numbers divided by N)
+  solution -
+        Scanner sc = new Scanner(System.in);
+        double  a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c= sc.nextDouble();
+        double num = (a + b + c)/ 3;
+        System.out.println(num);
+       */
+
+        /*Question2:
+        Inaprogram,input the side of a square. You have to output the area of the square.
+        (Hint : area of a square is (side x side))
+        solution -
+        float side = 2.3f;
+        int area = (int)(side * side);
+        System.out.print("area of Square is = ");
+        System.out.println(area);
+
+         */
+        /*uestion3:
+        Enter cost of 10 items from the user(using float datatype)
+        You have to output the total cost of the items back to the user as their bill.
+        (Add on : You can also try adding 18% gst tax to the items in the bill as an advanced problem
+        soloution 3 -
+        Scanner sc = new Scanner(System.in);
+        float one = sc.nextFloat();
+        float two = sc.nextFloat();
+        float three = sc.nextFloat();
+        float four = sc.nextFloat();
+        float five = sc.nextFloat();
+        float six = sc.nextFloat();
+        float seven = sc.nextFloat();
+        float eight = sc.nextFloat();
+        float nine = sc.nextFloat();
+        float ten = sc.nextFloat();
+
+        float total = one + two + three + four + five + six + seven + eight + nine +ten;
+        float gst = total *18 /100;
+        System.out.println("the  total cost of the item with gst of 18% is " + total);
+        System.out.println("gst is = "+  gst);
+
+        quesarion 5 -
+        int $ =23;
+        System.out.println($);
+
+                           SO THE 2NDDAY AND HERE WITH  VEDIO NO.4 AND  PRACTICE QUESATIONS ALSO COMPLETED
+         */
+    }
+}
+
+
+
+
+
