b12e9a0485cd33aa32b304af98c63856f6be023b
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-03 03:12:26 +0530

    PERFORMING ARITHMATIC OPERATOIRES IN JAVA-  JUNE 3

--- src/02_operators/operator.java
+++ src/02_operators/operator.java
@@ -2,9 +2,15 @@ import java.util.*;
 
 public class operator {
     public static void main(String args[]){
+
+        // USER SE INPUT LEKR OPERATOR SIKH RHA HU AND THIS CODE IS WORKING NOW IM
+        // CONVERTING THIS CODE INTO COMMENTS TOH STUDY NEXT TOPIC
         Scanner sc = new Scanner(System.in);
-
-
+        System.out.println("enter no. a");
+        int a  = sc.nextInt();
+        System.out.println("enter no b");-
+        int b = sc.nextInt();
+        System.out.println("ther sum of a and b =" + (a +b) );
 
     }
 }
