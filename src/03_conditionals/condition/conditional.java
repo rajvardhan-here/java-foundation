79f7a64dfbb87738291fbbec8d6c1ecc975daa1a
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-04 01:44:18 +0530

    PERFORMING A CONDITIONAL STATEMENT WITH A UNIQE EXAMPLE HAHAHA- JUNE 4

---
+++ src/03_conditionals/condition/conditional.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class conditional{
+    public static void main (String args[]){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("enter your age--");
+    int age = sc.nextInt();
+
+    if(age >= 5 ){
+        System.out.println("the girl age is gone for marriage ");
+    }
+    else{
+        System.out.println("you are eligible for marriage ");
+    }
+
+    }
+}
