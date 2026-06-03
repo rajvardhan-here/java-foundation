1ed27ab030aca2fbf9c6694b5d1e0d961f6a8cfd
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-03 05:36:42 +0530

    solving preactice quesation 1  -  JUNE 3

--- src/02_operators/operator.java
+++ src/02_operators/operator.java
@@ -41,8 +41,28 @@ public class operator {
 
         // TOPIC NO. 4   IS LOGICAL  OPERAOTR --
 
-        System.out.println( (2>4) || (0>1) );  // false
-        System.out.println( (9>7) || (1>3) );  // true\
+        //System.out.println( (2>4) || (0>1) );  // false
+        // System.out.println( (9>7) || (1>3) );  // true
+
+        // System.out.println( !(9>11 ));    //   true because it reverse the answerr
+
+        /*
+         TOPIC NO. 5 IS ASSIGNMENT  OPERAOTR --
+        int a = 10 ;
+        //a = a + 15;
+        a += 15;
+        System.out.println(a);  // 25
+        */
+
+        //PRACTICE QUESATION - 1
+
+        int x = 2 , y= 5;
+        int exp1 = (x * y / x);
+        int exp2 = (x * ( y / x) ) ;
+        System.out.println(exp1 + "," + exp2); // 5,4
+
+
+
 
 
 
