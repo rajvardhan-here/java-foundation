f0eb9c1885b3c4da54a76be1f146b853c9e67404
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-04 03:53:11 +0530

    Question2: Finish the following code so that it prints You have a fever if your temperature is- JUNE 4

--- src/03_conditionals/condition/practice.java
+++ src/03_conditionals/condition/practice.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class practice{
     public static void main (String args[]){
 
+        /*
         // Question1: Write a Java program to get anumber from the user and print whether it is positive or negative.
-
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         if (num >= 0){
@@ -12,6 +12,19 @@ public class practice{
         else{
             System.out.println("this number is negative number");
         }  // input 2 -  this number is psitive number
+
+         */
+
+
+        // Question2: Finish the following code so that it prints You have a fever if your temperature is
+        // above 100 and otherwise prints You don't have a fever
+        double temp = 103.05;
+        if (temp >100){
+            System.out.println("i have a fever ");
+        }
+        else {
+            System.out.println("i don't have a fever ");
+        } // i have a fever
 
 
     }
