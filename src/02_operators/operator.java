c6f3db39910fc19e8a027232239142b08e6eb72b
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-03 05:48:52 +0530

    solving preactice quesation 2  -  JUNE 3

--- src/02_operators/operator.java
+++ src/02_operators/operator.java
@@ -54,21 +54,27 @@ public class operator {
         System.out.println(a);  // 25
         */
 
-        //PRACTICE QUESATION - 1
-
+        /*PRACTICE QUESATION - 1
         int x = 2 , y= 5;
         int exp1 = (x * y / x);
         int exp2 = (x * ( y / x) ) ;
         System.out.println(exp1 + "," + exp2); // 5,4
+         */
 
+        /*
+        PRACTICE QUESATION - 2   output = JAVA
 
+        int x = 200, y = 50, z = 100;
+        if(x > y && y > z){
+            System.out.println("Hello");
+        }
+        if(z > y && z < x){
+            System.out.println("Java");
+        }
+        if((y+200) < x && (y+150) < z){
+            System.out.println("Hello Java");
+        }
 
-
-
-
-
-
-
-
+         */
     }
 }
