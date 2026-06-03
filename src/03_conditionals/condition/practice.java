26bff37e2f202c258fae87a5bd300958a448c232
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-04 04:06:02 +0530

    Question 3:Write a Java program to input week number(1-7) and print day of week name using switch case. - JUNE 4

--- src/03_conditionals/condition/practice.java
+++ src/03_conditionals/condition/practice.java
@@ -13,8 +13,6 @@ public class practice{
             System.out.println("this number is negative number");
         }  // input 2 -  this number is psitive number
 
-         */
-
 
         // Question2: Finish the following code so that it prints You have a fever if your temperature is
         // above 100 and otherwise prints You don't have a fever
@@ -25,6 +23,37 @@ public class practice{
         else {
             System.out.println("i don't have a fever ");
         } // i have a fever
+
+
+        // Question 4 :What will be the value of x & y in the following program
+        int a = 63, b = 36;
+        boolean x = (a < b) ? true : false;
+        int y= (a > b ) ? a : b;        // false  // y= 63
+         */
+
+        // Question 3:Write a Java program to input week number(1-7) and print day of week name using switch case.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the week number to know the day ");
+        int week = sc.nextInt();
+
+        switch (week){
+            case 1 : System.out.println("monday");
+            break;
+            case 2 : System.out.println("TUESDAY");
+            break;
+            case 3 : System.out.println("WEDNESDAY");
+                break;
+                case 4 : System.out.println("THURSDAY");
+                    break;
+                    case 5 : System.out.println("FRIDAY");
+                        break;
+                        case 6 : System.out.println("SATURDAY");
+                            break;
+                            case 7 : System.out.println("SUNDAY");
+        }  // input 4 --- THURSDAY
+
+
 
 
     }
