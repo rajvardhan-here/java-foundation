9eb9243fd73c1d7d84af7bd5bce532d916292b3b
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-04 01:50:15 +0530

    PERFORMING if and if  and  else statement - JUNE 4

--- src/03_conditionals/condition/conditional.java
+++ src/03_conditionals/condition/conditional.java
@@ -1,17 +1,22 @@
 import java.util.*;
 public class conditional{
     public static void main (String args[]){
-    Scanner sc = new Scanner(System.in);
+     //  if else conditions
+     Scanner sc = new Scanner(System.in);
 
     System.out.println("enter your age-");
     int age = sc.nextInt();
 
-    if(age >= 5 ){
+    if(age < 5 ){
         System.out.println("the girl age is gone for marriage ");
     }
+    if (age > 10 && age < 15){
+        System.out.println("kuch kam ki nhi h -");
+        }
     else{
         System.out.println("you are eligible for marriage ");
     }
+
 
     }
 }
