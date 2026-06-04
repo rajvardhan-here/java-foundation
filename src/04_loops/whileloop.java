cf055ffa39b3fbd427736918e53e4a80780b35e1
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-05 01:04:46 +0530

    // PRINT A NUMBER 1 TO N

--- src/04_loops/whileloop.java
+++ src/04_loops/whileloop.java
@@ -12,18 +12,26 @@ public class whileloop {
             counter ++;
         }
         System.out.println("RAJ is printed 100 time ");
-         */
 
         // PRINT A NUMBER 1 TO 10 USING WHILE LOOP
         int counter = 1;
 
-        while(counter <= 10){
+        while(counter < 100){
             System.out.print(" " + counter);
             counter ++;
         }
+        */
 
+        //print a nu,mber 1 to n
+        System.out.println("enter the number ");
+        int number = sc.nextInt();
+        int counter = 0;
 
-        //
+        while ( counter <= number ){
+            System.out.print(counter+ "  ");
+            counter ++;  //0  1  2  3  4  5  6  7  8  9  10  11
+
+        }
 
 
 
