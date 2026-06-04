e8721a65224f4ce962e184419a6cc7ad4e7c5d66
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-05 03:47:49 +0530

    // DISPLAY ALL THE NUMBER ENTER BY USER EXPECT MULTIPLE IF 10

--- src/04_loops/dowhile.java
+++ src/04_loops/dowhile.java
@@ -5,6 +5,7 @@ public class dowhile {
 
         Scanner sc = new Scanner(System.in);
 
+        /*
         // KEEP ENTERING NUMBER UNTIL USERS ENTER MULTIPLE OF 10
         do {
             System.out.println("enter the number ");
@@ -14,5 +15,17 @@ public class dowhile {
                 break;
             }
         }while (true) ;
+
+         */
+        // DISPLAY ALL THE NUMBER ENTER BY USER EXPECT MULTIPLE IF 10
+
+        do {
+            System.out.println("enter your number ");
+            int counter = sc.nextInt();
+            if (counter % 10 == 0) {
+                continue;
+            }
+            System.out.println("your number was - "+ counter);
+        }while (true);
     }
 }
