9a2c1baef3642e38f948c4c044ce49d15c7dee53
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-05 00:52:54 +0530

    // PRINT A NUMBER 1 TO 10 USING WHILE LOOP

--- src/04_loops/whileloop.java
+++ src/04_loops/whileloop.java
@@ -5,12 +5,28 @@ public class whileloop {
 
         Scanner  sc = new Scanner(System.in);
 
-        // WHILE LOOP K BASICS
+        /* WHILE LOOP K BASICS
         int counter = sc.nextInt();
         while(counter < 100){
             System.out.println(" RAJ ");
             counter ++;
         }
         System.out.println("RAJ is printed 100 time ");
+         */
+
+        // PRINT A NUMBER 1 TO 10 USING WHILE LOOP
+        int counter = 1;
+
+        while(counter <= 10){
+            System.out.print(" " + counter);
+            counter ++;
+        }
+
+
+        //
+
+
+
+
     }
 }
