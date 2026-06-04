4882a25e8e9129d728ab25f4f5d47412436721b9
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-05 01:55:41 +0530

    // print SQUARTE PATTERN

--- src/04_loops/forloop.java
+++ src/04_loops/forloop.java
@@ -3,13 +3,29 @@ import java.util.*;
 public class forloop {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-
+        /*
         // studying for loop
         // for ( initialisation; condition ; updation)
-
         for( int i = 1; i <= 10; i++){
             System.out.print(" " + i);   // 1 2 3 4 5 6 7 8 9 10
         }
+        */
+
+        // print SQUARTE PATTERN
+        for (int line = 1; line <= 5 ; line ++ ){
+            System.out.println(" * * * * * * * = " + line);
+
+            //* * * * * * * = 1
+            //* * * * * * * = 2
+            //* * * * * * * = 3
+            //* * * * * * * = 4
+            //* * * * * * * = 5
+
+        }
+
+
+
+
     }
 
 }
