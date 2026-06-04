71cf21fd6317189ea3caf377dd8164212504e9ae
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-05 03:00:16 +0530

    // reverse the given number

--- src/04_loops/forloop.java
+++ src/04_loops/forloop.java
@@ -9,7 +9,6 @@ public class forloop {
         for( int i = 1; i <= 10; i++){
             System.out.print(" " + i);   // 1 2 3 4 5 6 7 8 9 10
         }
-        */
 
         // print SQUARTE PATTERN
         for (int line = 1; line <= 5 ; line ++ ){
@@ -20,6 +19,17 @@ public class forloop {
             //* * * * * * * = 3
             //* * * * * * * = 4
             //* * * * * * * = 5
+
+            */
+
+        // reverse the givren number
+         int n = 12345;
+         int reverse =0;
+         for ( ;  n > 0 ; n= n / 10 ){
+             int digit = n % 10;
+             reverse = (reverse * 10) + digit;
+             System.out.println(" the reverse number is- " + reverse);  // the reverse number is - 54321
+
 
         }
 
