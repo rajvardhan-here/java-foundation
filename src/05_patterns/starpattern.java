a2d9a6f721cc42845c8810198928994fd52a9f43
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-06 04:47:50 +0530

    PRINT HALF PYRAMID PATTERN

--- src/05_patterns/starpattern.java
+++ src/05_patterns/starpattern.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class starpattern {
     public static void main(String args[]){
 
+        /*
         //"PRINT STAR PATTERN")
         for ( int line = 1; line <= 5 ; line ++){
             for( int star = 1; star <= line ; star++){
@@ -17,5 +18,39 @@ public class starpattern {
             //*****
 
         }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the number of line");
+        int n = sc.nextInt();
+
+        for ( int line = 1; line <= 4 ; line ++) {
+            for (int star = 1; star <= n - line; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        */
+        //*****
+        //****
+        //***
+        //**
+        //*
+
+        // PRINT HALF PYRAMID PATTERN
+        int n=4;
+        for (int line = 1 ; line <= n; line++){
+            for (int num = 1 ; num<=line  ; num++){
+                System.out.print(num);
+            }
+            System.out.println( );
+        }
+        //1
+        //12
+        //123
+        //1234
+
+
+
     }
+
 }
