9ea332079ee201941518218813e312cd7ad5d96d
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-06 02:20:13 +0530

    PRINT STAR PATTERN

--- src/05_patterns/starpattern.java
+++ src/05_patterns/starpattern.java
@@ -3,8 +3,19 @@ import java.util.*;
 public class starpattern {
     public static void main(String args[]){
 
-        //"starting pattern concept")
-       for ( int i = 1; i <= 5 ; i ++)
-           System.out.println("starting pattern concept");
+        //"PRINT STAR PATTERN")
+        for ( int line = 1; line <= 5 ; line ++){
+            for( int star = 1; star <= line ; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+            //*
+            //**
+            //***
+            //****
+            //*****
+
+        }
     }
 }
