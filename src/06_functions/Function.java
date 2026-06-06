8659a2740fa58e76d15fac2738bc694c8eb4fbe2
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-07 02:11:41 +0530

    STUDYING FUNCTION FROM SCRATCH - 7 JUNE

---
+++ src/06_functions/Function.java
@@ -0,0 +1,17 @@
+public class Function {
+
+
+    // "STUDYING FUNCTION FROM SCRATCH - 7 JUNE"
+    public static void helloraj(){
+        int i = 24;
+        System.out.println("hello raj");
+        System.out.println("hello raj");
+        System.out.println("hello raj");
+        System.out.println(i);
+        return;
+    }
+
+    public static void main(String[] args){
+        helloraj();
+    }
+}
