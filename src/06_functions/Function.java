085ed1b87df4a7e1892f80c9bdeab734091b8269
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-07 03:16:59 +0530

    // parameter and  arguments  in function - 7 JUNE

--- src/06_functions/Function.java
+++ src/06_functions/Function.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
 public class Function {
 
 
+    /*
     // "STUDYING FUNCTION FROM SCRATCH - 7 JUNE"
     public static void helloraj(){
         int i = 24;
@@ -10,8 +13,25 @@ public class Function {
         System.out.println(i);
         return;
     }
-
     public static void main(String[] args){
         helloraj();
+        */
+
+
+
+        // parameter and  arguments  in function
+    public static void calculatesum( int a , int b){
+            int sum = a+b;
+        System.out.println("sum is : " + sum);
+            return ;
+        }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number a :");
+        int a = sc.nextInt();
+        System.out.println("enter number b :");
+        int b = sc.nextInt();
+        calculatesum(a,b);
     }
+
 }
