e247f05c2e0eb4d20c44c9f4a1ea550c266bd197
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-08 02:01:56 +0530

    //  PRINT ALL THE PRIME NUMBER IN RANGE - 8  JUNE

--- src/06_functions/primenumber.java
+++ src/06_functions/primenumber.java
@@ -15,12 +15,20 @@ public class primenumber {
         return true;                                // true dede
     }
 
+    //  PRINT ALL THE PRIME NUMBER IN RANGE
+    public static void primeinrange(int n){
+        for (int i = 2 ; i<= n; i++){
+            if(isPrime(i)){
+                System.out.print(i+ " ");
+            }
+        }System.out.println( );
+    }
 
     public static void main(String args[]){         //  MAIN FUNCTION
         Scanner sc = new Scanner(System.in);
         System.out.println("ENTER YOUR NUMBER---: ");
         int n = sc.nextInt();
-        System.out.print(isPrime(7));            // 7 id prime ,,done
+        primeinrange(n);            // 7 id prime ,,done
     }
 
 }
