d82ddd64c52063b200d47434b1cdeb7b3ea380ea
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-09 03:12:59 +0530

    Question2:In a program ,input the side of a square.You have to output the area of the square

--- src/Practice/Session1.java
+++ src/Practice/Session1.java
@@ -4,6 +4,7 @@ public class Session1 {
     public static void main(String args[]){
 
         // practice session 1
+        /*
         // Question1:In a program , input 3 numbers :A,B and C.You have to output the average of these 3 numbers.
 
         int a = 21;
@@ -11,5 +12,16 @@ public class Session1 {
         int c = 11;
         int sum = a+b+c/3;
         System.out.println("the sum of a b and c is -: " +sum);
+         */
+
+        // Question2:In a program ,input the side of a square.You have to output the area of the square
+        /*
+        int side = 21;
+        System.out.println("area of sqaure =  " +(side * side));   //441
+        */
+
+
+
+
     }
 }
