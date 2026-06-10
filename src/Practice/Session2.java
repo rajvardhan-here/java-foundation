a3a22461b91b3f0950d1bcf1c56f82965c65a13c
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-11 03:50:24 +0530

    decimal to binary number

--- src/Practice/Session2.java
+++ src/Practice/Session2.java
@@ -137,6 +137,7 @@ import java.util.*;
         // -----------------------------------------------------------------------------------------------------------------------------
 
 
+        /*
         public static void bintodec(int binnum) {
             int mynum = binnum;
             int pow = 0;
@@ -152,12 +153,36 @@ import java.util.*;
         }
         public static void main(String args []){
             Scanner sc = new Scanner(System.in);
-            System.out.println("enter your number = ");       //105
+            System.out.println("enter your number = ");       //1001
             int binnum = sc.nextInt();
             bintodec(binnum);    // 5
         }
+        */
 
 
+// -----------------------------------------------------------------------------------------------------------------------------
+
+
+        public static void dectobin(int decnum) {
+            int mynum = decnum;
+            int pow = 0;
+            int bin =0;
+            while (decnum >0){
+                int lastdigit = decnum % 2;
+                bin = bin  + (lastdigit * (int) Math.pow(10,pow));
+                decnum = decnum/2;
+                pow++;
+
+            }
+            System.out.println("decimal of " +mynum + " is " +bin);
+        }
+        public static void main(String args []){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter your number = ");       //11
+            int decnum = sc.nextInt();
+            dectobin(decnum);                         //1011
+
+        }
 
 
     }
