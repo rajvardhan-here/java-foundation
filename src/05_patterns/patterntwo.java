a6c6105ca318499d3a0ac330512348eaf9fd6988
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-11 08:02:25 +0530

    //// print floyd_triangle

--- src/05_patterns/patterntwo.java
+++ src/05_patterns/patterntwo.java
@@ -62,10 +62,10 @@ public class patterntwo {
 
     // -----------------------------------------------------------------------------------------------------------------------------------
 
-    // INVERTED HALF PYRAMID PATTERN
+    // INVERTED HALF PYRAMID PATTERN  WITH NUMBERS
 
+    /*
     public static void inverted_HAlFPYRAMID(int rows ){
-
         for (int i = 1; i <= rows ; i++){
 
             for (int j = 1 ; j <= rows - i + 1 ; j++ ){
@@ -78,17 +78,41 @@ public class patterntwo {
     public static void main (String args []){
 
         inverted_HAlFPYRAMID(5);
-
     }
+
     //1 2 3 4 5
     //1 2 3 4
     //1 2 3
     //1 2
     //1
 
+     */
+
 
     // -----------------------------------------------------------------------------------------------------------------------------------
 
+    // print floyd_triangle
+    public static void floyd_triangle(int rows ){
+        int counter = 1;
+        for (int i = 1; i <= rows ; i++){
+
+            for (int j = 1 ; j <= i ; j++ ){
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main (String args []){
+
+        floyd_triangle(5);
+    }
+    //1
+    //2 3
+    //4 5 6
+    //7 8 9 10
+    //11 12 13 14 15
 
 
 
