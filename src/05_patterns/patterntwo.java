c3d56d539a3d23d5e24e49baf9d8c54b2997224c
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-11 06:14:49 +0530

    PRINT HOLLOW  RECTANGLE  PATTERN

---
+++ src/05_patterns/patterntwo.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class patterntwo {
+
+
+        //"PRINT HOLLOW  RECTANGLE  PATTERN")
+    public static void hollow(int totlrows , int totlcol){
+        for ( int i = 1; i <= totlrows ; i ++) {     // outer loop
+
+            for (int j = 1; j  <= totlcol ; j++) {    //inner loop
+                if(i  == 1 ||  i == totlrows || j ==1 || j == totlcol){
+                System.out.print("* ");
+              }
+                else {
+                System.out.print("  ");
+                }
+            }
+         System.out.println();
+        }
+    }
+    public static void main(String args[]){
+    hollow(5,20);
+    }
+            // * * * * * * * * * * * * * * * * * * * *
+            // *                                     *
+            // *                                     *
+            // *                                     *
+            // * * * * * * * * * * * * * * * * * * * *
+
+}
