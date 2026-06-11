d6c42ea674a0de172de822e0f418830d2a89ad11
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-11 06:58:58 +0530

    // half pyramid and inverted pattern

--- src/05_patterns/patterntwo.java
+++ src/05_patterns/patterntwo.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class patterntwo {
 
 
-        //"PRINT HOLLOW  RECTANGLE  PATTERN")
+    /*   "PRINT HOLLOW  RECTANGLE  PATTERN")
     public static void hollow(int totlrows , int totlcol){
         for ( int i = 1; i <= totlrows ; i ++) {     // outer loop
 
@@ -21,10 +21,48 @@ public class patterntwo {
     public static void main(String args[]){
     hollow(5,20);
     }
+    */
             // * * * * * * * * * * * * * * * * * * * *
             // *                                     *
             // *                                     *
             // *                                     *
             // * * * * * * * * * * * * * * * * * * * *
+
+    // -----------------------------------------------------------------------------------------------------------------------------------
+
+    // half pyramid and inverted pattern
+    public static void HAlFPYRAMID(int rows ){
+
+        for (int i = 1; i <= rows ; i++){
+
+            for (int j = 1 ; j <=  rows - i ; j++ ){
+                System.out.print(" ");
+            }
+            for (int j = 1 ; j <= i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main (String args []){
+
+        HAlFPYRAMID(6);
+
+    }
+    //     *
+    //    **
+    //   ***
+    //  ****
+    // *****
+    //******
+
+    // -----------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
 
 }
