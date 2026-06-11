41e91617315d93bcbc61947f20301c292253f487
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-12 04:02:27 +0530

    // HOW TO CREATE AN ARRAY -  JUNE 12

--- src/07_arrays/array.java
+++ src/07_arrays/array.java
@@ -1,7 +1,24 @@
 import java.util.*;
 
-public class array {
+public class array   {
     public static void main (String args[]){
-        System.out.println("raj");
+
+        // create  an array
+         int numbers[] =  new int[100];
+
+         Scanner sc = new Scanner(System.in);
+
+         numbers [0] =  sc.nextInt();
+         numbers [1] =  sc.nextInt();
+         numbers [2] =  sc.nextInt();
+
+        System.out.println("no. of numbers in java   = " +numbers[0]);
+        System.out.println("no. of numbers in  c      = " +numbers[1]);
+        System.out.println("no. of numbers in  python = " +numbers[2]);
+
+        System.out.println(numbers.length);
+
+        numbers[2] = 1001;
+        System.out.println(numbers[2]);
     }
 }
