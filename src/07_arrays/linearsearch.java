0e68d0b6b2649aa8772812aaac58d028ec601b9a
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-12 08:51:17 +0530

    // LARGEST AND SMALLEST OF THE NUMBER  IN ARRAY -  JUNE 12

--- src/07_arrays/linearsearch.java
+++ src/07_arrays/linearsearch.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class linearsearch {
 
+    /*
     public static void main(String[] args) {                 // [] lagaya
 
         int marks[] = {2, 10, 6, 8, 10, 12, 10, 16};
@@ -15,6 +16,7 @@ public class linearsearch {
         }
     }
 
+
     public static int linearsearch(int marks[], int key) {
 
         for (int i = 0; i < marks.length; i++) {
@@ -24,4 +26,34 @@ public class linearsearch {
         }
         return -1;
     }
+
+        */
+
+//     -------------------------------------------------------------------------------------------------------------
+
+
+    public static void main(String[] args) {
+        int[] numbers = { 112, 4, 2, 12, 31, 21, 4123, 31, 1 };
+        System.out.println("Largest: " + largestNumber(numbers));
+        System.out.println("Smallest: " + smallestNumber(numbers));
+    }
+
+    public static int largestNumber(int[] numbers) {
+        int largest = Integer.MIN_VALUE;
+        for (int num : numbers) {
+            if (num > largest) largest = num;
+        }
+        return largest;
+    }
+
+    public static int smallestNumber(int[] numbers) {
+        int smallest = Integer.MAX_VALUE;
+        for (int num : numbers) {
+            if (num < smallest) smallest = num;
+        }
+        return smallest;
+    }
+    //Largest: 4123
+    //Smallest: 1
+
 }
