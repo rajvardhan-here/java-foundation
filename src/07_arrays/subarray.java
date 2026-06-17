bc501b6b714e2e5de974e7358a9fb4db04eba84e
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-17 07:49:24 +0530

    PRINT SUBARRAY IN ARRAY   - JUNE 17

---
+++ src/07_arrays/subarray.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class subarray {
+    public static void subary(int numbers[]){
+        int tp= 0;
+        for (int i =0 ; i <numbers.length ; i++){
+            int start = i;
+            for (int j = i; j< numbers.length ;j++){
+              int end = j   ;
+
+              for (int k = start ; k <=  end ;k++){
+                  System.out.print(numbers[k]+ " ");
+                  tp++;
+              }
+              System.out.println();
+        }
+            System.out.println();
+        }
+        System.out.print("total pairs = " +tp);
+    }
+
+
+    public static void main(String args[]){
+        int numbers[] = {2,4,6,8};
+
+        subary(numbers);
+    }
+    //2
+    //2 4
+    //2 4 6
+    //2 4 6 8
+    //
+    //4
+    //4 6
+    //4 6 8
+    //
+    //6
+    //6 8
+    //
+    //8
+}
