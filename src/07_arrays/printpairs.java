c31d95a249ded509df68747b57e70ec72becfb2c
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-17 07:03:47 +0530

    //  PRINT PAIR OF THE NUMBER IN AN ARRAY  - JUNE 17

---
+++ src/07_arrays/printpairs.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class printpairs{
+    public  static  int pairs(int numbers[]){
+      for(int i = 0 ; i < numbers.length; i++){
+          int curr = numbers[i];
+          for (int j = i+1 ; j<numbers.length ; j++ ){
+              System.out.print("(" +curr+ "," +numbers[j]+ ")");
+          }
+          System.out.println();
+      }
+      return 1;
+    }
+
+    public static void main(String args[]){
+        int numbers[] = {2,4,6,8,10};
+
+        pairs(numbers);
+    }
+    //(2,4)(2,6)(2,8)(2,10)
+    //(4,6)(4,8)(4,10)
+    //(6,8)(6,10)
+    //(8,10)
+}
