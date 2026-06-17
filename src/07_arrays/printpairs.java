88d4a6ef81d9d38b7afbbe2e3c1f95392edd6144
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-06-17 07:20:35 +0530

    PRACTICE DONE FOR THE PAIR IN ARRAY   - JUNE 17

--- src/07_arrays/printpairs.java
+++ src/07_arrays/printpairs.java
@@ -1,29 +1,23 @@
 import java.util.*;
 
-public class printpairs{
-    public  static  int pairs(int numbers[]){
-        int tp =0;
-      for(int i = 0 ; i < numbers.length; i++){
-          int curr = numbers[i];
-          for (int j = i+1 ; j<numbers.length ; j++ ){
-              System.out.print("(" +curr+ "," +numbers[j]+ ")");
-              tp++;
-          }
-          System.out.println();
-      }
-      System.out.print("total pairs = " +tp);
-      return 1;
+public class printpairs {
+    public static void pairs(int numbers[]){
+        int tp= 0;
+        for (int i =0 ; i<numbers.length ; i++){
+            int curr = numbers[i];
+            for (int j = i+1; j< numbers.length ;j++){
+                System.out.print("(" +curr+ "," +numbers[j]+ ")" );
+                tp++;
+            }
+        System.out.println();
+        }
+        System.out.println("total pairs = " +tp);
     }
 
+
     public static void main(String args[]){
-        int numbers[] = {2,4,6,8,10};
+        int numbers[] = {2,4,6,7,8,9,};
 
         pairs(numbers);
     }
-    //(2,4)(2,6)(2,8)(2,10)
-    //(4,6)(4,8)(4,10)
-    //(6,8)(6,10)
-    //(8,10)
-
-    // total pairs = 10
 }
