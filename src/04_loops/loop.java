8ee1d01c01b44143e9b754977dae558464229504
Rajvardhan <rajvardhansinghchouhan1@gmail.com>
2026-09-23 02:49:51 +0530

    print star patten

--- src/04_loops/loop.java
+++ src/04_loops/loop.java
@@ -44,7 +44,7 @@ public class loop {
         int n = sc.nextInt();
 
         for(int i = 1 ; i <=10; i++){
-            System.out.println(n +" " +"* "  +i  +" " +"=" +n*i);
+            System.out.println(n +" * "  +i   +" = " +n*i);
         }
 
     }
---
+++ src/05_patterns/patternone.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class patternone {
+
+    public static void main(String args[]){
+
+            for (int i = 1 ; i <= 4  ; i++) {
+                for (int star = 1; star <= i; star++) {
+                    System.out.print("*");
+                }
+                System.out.println();            }
+    }
+}
