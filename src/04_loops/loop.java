dbe3752ad2374dfc730ae06e140fabeb5e03d5fd
Rajvardhan <rajvardhansinghchouhan1@gmail.com>
2026-09-23 00:42:23 +0530

    sum natural num

--- src/04_loops/loop.java
+++ src/04_loops/loop.java
@@ -3,14 +3,16 @@ import java.util.*;
 public class loop {
     public static void main(String args[]){
 
+        System.out.print("enter the number n");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int number = 1;
-        while( number <= n){
-
-            System.out.print(number +" " );
-            number++;
+        int sum  = 0;
+        int i = 1;
+        while( i <= n){
+            sum = sum+i;
+            System.out.print(sum +" " );
+            i++;
 
         }
     }
