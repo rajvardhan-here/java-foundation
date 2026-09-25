2b9970f280c2a7ee92137e287e45f72292c6fbf9
Rajvardhan <rajvardhansinghchouhan1@gmail.com>
2026-09-25 23:34:13 +0530

    Auto commit

--- src/05_patterns/patternone.java
+++ src/05_patterns/patternone.java
@@ -3,13 +3,14 @@ public class patternone {
 
     public static void main(String args[]){
 
-        int n = 4;
-        for (int i = 1 ; i <= n ; i++){
-            for (int j = 1 ; j <= n -i +1; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+     int n = 4;
+     for (int line = 1; line <= n ; line++){
+         for(int j = 1 ; j <=  n -line +1 ; j++){
+             System.out.print("*");
+         }
+         System .out.println();
+
+     }
 
 
     }
