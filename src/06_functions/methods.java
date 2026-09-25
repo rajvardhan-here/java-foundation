5b43656519dc10634c0989721a3360fabaf6806d
Rajvardhan <rajvardhansinghchouhan1@gmail.com>
2026-09-26 00:54:42 +0530

    Auto commit

--- src/06_functions/methods.java
+++ src/06_functions/methods.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class methods {
 
-    public static int raj(int a , int b){
-        a+b;
+    public static int raj(a, b ){
+        a
         return;
     }
 
 
-    public static void main(String argsp[]){
+    public static void raj(String argsp[]){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
