df276d78e9b9bd76dbc6cc03314c314e57667419
Rajvardhan <rajvardhansinghchouhan1@gmail.com>
2026-09-26 00:53:12 +0530

    Auto commit

---
+++ src/06_functions/methods.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class methods {
+
+    public static int raj(int a , int b){
+        a+b;
+        return;
+    }
+
+
+    public static void main(String argsp[]){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println(raj);
+
+    }
+}
