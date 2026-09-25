f6d9ea93fffb7c3985a715b986faa3002b918bb1
Rajvardhan <rajvardhansinghchouhan1@gmail.com>
2026-09-26 00:40:37 +0530

    Auto commit

--- src/05_patterns/patternone.java
+++ src/05_patterns/patternone.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class patternone {
 
-    public static void main(String args[]){
+
 /*
      int n = 4;
      char c = 'A';
@@ -22,7 +22,8 @@ public class patternone {
 
 
 
-         
+    public static void main(String args[]){
+        
 
     }
 }
