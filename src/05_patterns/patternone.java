ed2074919a2292ff3accfaf5a04da8785d790e5f
Rajvardhan <rajvardhansinghchouhan1@gmail.com>
2026-09-25 19:39:41 +0530

    print nverted star patttern

--- src/05_patterns/patternone.java
+++ src/05_patterns/patternone.java
@@ -3,10 +3,14 @@ public class patternone {
 
     public static void main(String args[]){
 
-            for (int i = 1 ; i <= 4  ; i++) {
-                for (int star = 1; star <= i; star++) {
-                    System.out.print("*");
-                }
-                System.out.println();            }
+        int n = 4;
+        for (int i = 1 ; i <= n ; i++){
+            for (int j = 1 ; j <= n -i +1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
     }
 }
